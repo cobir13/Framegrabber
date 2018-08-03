338aa047ac446df54a4e7ae7a48fd59fead974b9
cobir13 <41207006+cobir13@users.noreply.github.com>
2018-08-03 12:09:50 -0500

    Add python scripts; fix version checker

---
+++ fgterm.py
@@ -0,0 +1,28 @@
+import zmq
+
+hostname = "tcp://localhost:5555"
+
+context = zmq.Context()
+print("Connecting")
+socket = context.socket(zmq.REQ)
+socket.RCVTIMEO = 1000
+socket.connect(hostname)
+socket.setsockopt(zmq.LINGER, 1000)
+print("Connected to {}".format(hostname))
+
+print("Ready for input")
+done = False
+while not done:
+    print("> ", end="")
+    i = input()
+    socket.send(i.encode('utf-8'))
+
+    try:
+        msg = socket.recv()
+        print(msg.decode('utf-8'))
+    except zmq.error.Again:
+        print("Timeout")
+        done = True
+
+socket.close()
+print("Disconnected from {}".format(hostname))
